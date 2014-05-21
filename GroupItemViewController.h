644fd3f81831a6cb630339f5b9492bc835d70a92
John Battaglia <jbatts82@hotmail.com>
2014-05-21 13:35:10 -0400

    5/21/14 commit

--- GroupItemViewController.h
+++ GroupItemViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface GroupItemViewController : UITableViewController<NSURLConnectionDelegate,NSXMLParserDelegate>
+@interface GroupItemViewController : UITableViewController
 
 
 
