34bd7ac9f618707bde828a95e6b43cf40b7c2447
John Battaglia <jbatts82@hotmail.com>
2014-05-21 23:36:59 -0400

    Commit to GitHub

--- GroupItemViewController.m
+++ GroupItemViewController.m
@@ -5,7 +5,7 @@
 //  Created by John Battaglia on 5/20/14.
 //  Copyright (c) 2014 John Battaglia. All rights reserved.
 //
-
+//
 #import "GroupItemViewController.h"
 
 @interface GroupItemViewController ()
